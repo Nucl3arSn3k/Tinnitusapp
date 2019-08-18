051df788b8c0cfffdc2485ed00c566b0bd582571
Nucl3ar Sn3k <shredderofwheat@gmail.com>
2019-08-18 18:35:54 -0400

    Functioning TOS screen
    Wow,I've done it.

---
+++ lib/homepage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:ownthetone/Constants.dart';
+import 'package:ownthetone/pages/FirstScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:ownthetone/main.dart';
+
+void main() =>
+    runApp(MaterialApp(title: "Own the Tone", home: MainPersistentTabBar2()));
+
+class MainPersistentTabBar2 extends StatefulWidget {
+  @override
+  MainPersistentTabBarState2 createState() => MainPersistentTabBarState2();
+  
+}
+
+
+
+class MainPersistentTabBarState2 extends State<MainPersistentTabBar2> {
+  Brightness brightness;
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: <Widget>[
+                Container(
+                  width: 90,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text("Session 1"),
+                ),
+                Container(
+                  width: 90,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text("Session 2"),
+                ),
+                Container(
+                  width: 90,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text("Session 3"),
+                ),
+                Container(
+                  width: 90,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: Text("Session 4"),
+                ),
+              ],
+            ),
+            title: Text('Own The Tone '),
+            actions: <Widget>[
+              PopupMenuButton<String>(
+                onSelected: _choiceAction,
+                itemBuilder: (BuildContext context) {
+                  return Constants.choices.map((String choice) {
+                    return PopupMenuItem<String>(
+                      value: choice,
+                      child: Text(choice),
+                    );
+                  }).toList();
+                },
+              )
+            ],
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              FirstScreen(),
+              Center(child: Text("Sample two")),
+              Center(child: Text("Sample three")),
+              Center(child: Text("Sample four")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // This area controls the settings menus
+  void _choiceAction(String choice) {
+    if (choice == Constants.about) {
+      Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('About the app'),
+          ),
+          body: new PageView(),
+        );
+      }));
+    } else if (choice == Constants.settings) {
+      Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Settings'),
+          ),
+          body: new Container(
+              child: Center(
+            child: Text('placeholder'),
+          )),
+        );
+      }));
+    }
+  }
+}
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,91 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:ownthetone/Constants.dart';
-import 'package:ownthetone/pages/FirstScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:firebase_analytics/observer.dart';
-import 'package:ownthetone/splashscreen.dart';
+import 'homepage.dart';
 
-void main() =>
-    runApp(MaterialApp(title: "Own the Tone", home: MainPersistentTabBar2()));
-
-class MainPersistentTabBar2 extends StatefulWidget {
-  @override
-  MainPersistentTabBarState2 createState() => MainPersistentTabBarState2();
   
+void main() =>
+    runApp(new MaterialApp(
+      title: "Own the Tone",
+      initialRoute: '/',
+      routes: {
+      '/': (context) => IntroScreen(),
+      '/second': (context) => MainPersistentTabBar2(),
+      }
+      ));
+
+class Splash extends StatefulWidget {
+@override
+SplashState createState() => new SplashState();
 }
 
+class SplashState extends State<Splash> {
+Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
 
-
-class MainPersistentTabBarState2 extends State<MainPersistentTabBar2> {
-  Brightness brightness;
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.dark,
-      ),
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: <Widget>[
-                Container(
-                  width: 90,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: Text("Session 1"),
-                ),
-                Container(
-                  width: 90,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: Text("Session 2"),
-                ),
-                Container(
-                  width: 90,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: Text("Session 3"),
-                ),
-                Container(
-                  width: 90,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: Text("Session 4"),
-                ),
-              ],
-            ),
-            title: Text('Own The Tone '),
-            actions: <Widget>[
-              PopupMenuButton<String>(
-                onSelected: _choiceAction,
-                itemBuilder: (BuildContext context) {
-                  return Constants.choices.map((String choice) {
-                    return PopupMenuItem<String>(
-                      value: choice,
-                      child: Text(choice),
-                    );
-                  }).toList();
-                },
-              )
-            ],
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              FirstScreen(),
-              Center(child: Text("Sample two")),
-              Center(child: Text("Sample three")),
-              Center(child: Text("Sample four")),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  // This area controls the settings menus
-  void _choiceAction(String choice) {
-    if (choice == Constants.about) {
-      Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text('About the app'),
-          ),
-          body: new PageView(),
-        );
-      }));
-    } else if (choice == Constants.settings) {
-      Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Settings'),
-          ),
-          body: new Container(
-              child: Center(
-            child: Text('placeholder'),
-          )),
-        );
-      }));
+    if (_seen) {
+    Navigator.of(context).pushReplacement(
+        new MaterialPageRoute(builder: (context) => new Home()));
+    } else {
+    prefs.setBool('seen', true);
+    Navigator.of(context).pushReplacement(
+        new MaterialPageRoute(builder: (context) => new IntroScreen()));
     }
-  }
 }
 
+@override
+void initState() {
+    super.initState();
+    new Timer(new Duration(milliseconds: 200), () {
+    checkFirstSeen();
+    });
+}
 
+@override
+Widget build(BuildContext context) {
+    return new Scaffold(
+    body: new Center(
+        child: new Text('Loading...'),
+    ),
+    );
+}
+}
 
+class Home extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+    return new Scaffold(
+    appBar: new AppBar(
+        title: new Text('Hello'),
+    ),
+    body: new Center(
+        child: new Text('This is the second page'),
+    ),
+    );
+}
+}
+
+class IntroScreen extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+    return new Scaffold(
+    body: new Center(
+        child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+            new Text('This is the placeholder for the TOS'),
+            new MaterialButton(
+            child: new Text('Go to Home Page'),
+            onPressed: () {
+                Navigator.of(context).pushReplacement(
+                     MaterialPageRoute(builder: (context) => MainPersistentTabBar2()));
+            },
+            )
+        ],
+        ),
+    ),
+    );
+}
+}
