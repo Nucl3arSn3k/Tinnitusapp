6c83cd0055b2b819117031e40b24f1d111b70770
Tormenator1 <shredderofwheat@gmail.com>
2021-01-28 13:39:43 -0500

    Updated to use embeding
    Still throws issues with work items,fails to process firebase preformance plugins

--- lib/UI/usagestats.dart
+++ lib/UI/usagestats.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:charts_flutter/flutter.dart';
+//import 'package:charts_flutter/flutter.dart';
 
 class Stats extends StatefulWidget {
   @override
